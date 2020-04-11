1d66caa201df1f678a8bcd5c43f67858153a8f2f
Alexandra Mironova <sominka1939@gmail.com>
2020-04-11 21:41:46 +0300

    Preparing to CI (AppVeyor)

---
+++ src/test/java/ru/netology/cardDeliveryTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class cardDeliveryTest {
+
+    /*void shouldTestingSamthing() {
+        open("http://localhost:9999");
+    }  */
+}
